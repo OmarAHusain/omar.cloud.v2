e8e688813c2433f281bf9770ed73bb20065cbf07
Omar Husain <omar.a.husain@gmail.com>
2022-07-12 12:52:36 +1000

    React-Icons fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "05a5853f7d0b5eed"; }
+/******/ 		__webpack_require__.h = function() { return "c5a5f955d212070d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
