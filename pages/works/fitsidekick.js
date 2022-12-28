4a1bbdec5fa6d742ae53815cd591d98419bda331
Omar Husain <omar.a.husain@gmail.com>
2022-12-29 00:21:33 +1100

    Minor change - Updating FitSideKick title

--- pages/works/fitsidekick.js
+++ pages/works/fitsidekick.js
@@ -12,7 +12,7 @@ import P from '../../components/paragraph'
 import Layout from "../../components/layouts/article";
 
 const Work = () => (
-    <Layout title="Generator">
+    <Layout title="FitSideKick">
         <Container>
             <Divider my={6} />
             <Title>
