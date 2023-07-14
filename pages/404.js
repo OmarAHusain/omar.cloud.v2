fe9d9b8bada597c30576b196c6577c43ced30dd3
Omar Husain <omar.a.husain@gmail.com>
2023-07-14 06:15:14 -0700

    Minor 404 update

--- pages/404.js
+++ pages/404.js
@@ -7,22 +7,25 @@ import {
     Divider,
     Button
 } from '@chakra-ui/react'
+import Section from '../components/section.js'
 
 const NotFound = () => {
     return (
         <Container>
-            <Heading as="h1">Still  building things out <br /> ---- <br/> Come back later! </Heading>
-            <Divider my={6} />
-            <Text>Error 404: Page doesn&apos;t exist... yet</Text>
-            
-            <Box my={6} align="center">
-                <NextLink href="/" passHref>
-                    <Button colorScheme="teal">Return to home</Button>
-                </NextLink>
-            </Box>
+            <Section>
+                <Heading as="h1">Still  building things out <br /> ---- <br/> Come back later! </Heading>
+                <Divider my={6} />
+                <Text>Error 404: Page doesn&apos;t exist... yet</Text>
+                
+                <Box my={6} align="center">
+                    <NextLink href="/" passHref>
+                        <Button colorScheme="teal">Return to home</Button>
+                    </NextLink>
+                </Box>
+            </Section>
         </Container>
     )
 }
 
 export default NotFound
-export { getServerSideProps } from '../../components/chakra'
+//export { getServerSideProps } from '../components/chakra'
